ac42effba2404c7f33ce00229983682cd0e554da
Asadullah Al Galib <aagalib2323@gmail.com>
2025-06-23 14:20:25 +0600

    create acceptGcc controller

--- server/controllers/gcc.controller.js
+++ server/controllers/gcc.controller.js
@@ -1,29 +1,32 @@
 import prisma from "../config/prisma";
 import { CustomError } from "../lib/customError";
 
-export const createGccRequest = async (req, res, next) => {
+export const requestGcc = async (req, res, next) => {
   try {
-    const { 
-      phoneNumber, 
-      productName, 
-      depositMethod, 
-      address, 
-      city, 
+    const {
+      phoneNumber,
+      productName,
+      depositMethod,
+      address,
+      city,
       estimatedDepositDateTime,
-      pickupPointId 
+      droppingPoint,
     } = req.body;
-    
+
     const userId = req.user.id;
     if (!phoneNumber || !depositMethod || !estimatedDepositDateTime) {
-      throw new CustomError('Missing required fields', 400);
+      throw new CustomError("Missing required fields", 400);
     }
 
-    if (depositMethod === 'DEPOSIT_FROM_HOME' && (!address || !city)) {
-      throw new CustomError('Address and city are required for home pickup', 400);
+    if (depositMethod === "DEPOSIT_FROM_HOME" && (!address || !city)) {
+      throw new CustomError(
+        "Address and city are required for home pickup",
+        400,
+      );
     }
 
-    if (depositMethod === 'DROP_AT_PUP' && !pickupPointId) {
-      throw new CustomError('Pickup point is required for drop-off', 400)
+    if (depositMethod === "DROP_AT_PUP" && !pickupPointId) {
+      throw new CustomError("Pickup point is required for drop-off", 400);
     }
 
     await prisma.gccRequest.create({
@@ -35,21 +38,66 @@ export const createGccRequest = async (req, res, next) => {
         address,
         city,
         estimatedDepositDateTime: new Date(estimatedDepositDateTime),
-        pickupPointId,
-      }
+        droppingPoint,
+      },
     });
 
     res.status(201).json({
       success: true,
-      message: 'GCC request created successfully',
+      message: "GCC request created successfully",
     });
   } catch (error) {
-    console.error('Error creating GCC request:', error);
+    console.error("Error creating GCC request:", error);
     next(error);
   }
 };
 
-
 export const acceptGccRequest = async (req, res, next) => {
+  try {
+    const { requestId } = req.params;
+    const { amount, fee } = req.body;
+    const gccRequest = await prisma.gccRequest.findUnique({
+      where: {
+        id: requestId,
+        deletedAt: null,
+      },
+    });
+    if (!gccRequest) {
+      throw new CustomError("Gcc request not found", 404);
+    }
+    if (gccRequest.status !== "PENDING") {
+      throw new CustomError("Request already processed", 400);
+    }
+    const validityStart = new Date();
+    const validityEnd = new Date(validityStart);
+    validityEnd.setMonth(validityEnd.getMonth() + 1);
+    await prisma.$transaction([
+      prisma.grayCacheCredit.create({
+        data: {
+          userId: gccRequest.userId,
+          amount,
+          fee,
+          validityStart,
+          validityEnd,
+          gccRequestId: gccRequest.id,
+        },
+      }),
+      prisma.gccRequest.update({
+        where: {
+          id: requestId,
+        },
+        data: {
+          status: "APPROVED",
+        },
+      }),
+    ]);
 
-}
+    res.status(200).json({
+      success: true,
+      message: "GCC Request Approved",
+    });
+  } catch (error) {
+    console.error("Error in accepting Gcc Req:", error);
+    next(error);
+  }
+};
