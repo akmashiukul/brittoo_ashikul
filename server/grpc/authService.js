301b712fd2810437ac24e876912dc06a41979eb5
Asadullah Al Galib <aagalib2323@gmail.com>
2025-10-08 05:03:26 +0600

    change bindasync in gRPC auth Service

--- server/grpc/authService.js
+++ server/grpc/authService.js
@@ -42,7 +42,7 @@ export const startAuthServer = () => {
     }
   });
   server.bindAsync(
-    "0.0.0.0:50051",
+    "127.0.0.1:50051",
     grpc.ServerCredentials.createInsecure(),
     () => {
       console.log("Auth gRPC server running on port 50051");
